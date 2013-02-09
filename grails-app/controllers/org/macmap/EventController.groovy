3929e23bad43c683e5eaf33926cf0da8e9e57c45
Brandon Maus <Zeth2302@gmail.com>
2013-02-08 22:15:40 -0600

    Added simple event add form.

--- grails-app/controllers/org/macmap/EventController.groovy
+++ grails-app/controllers/org/macmap/EventController.groovy
@@ -1,10 +1,23 @@
 package org.macmap
 
 class EventController {
-    def getEvents(){
+
+    def index() { }
+
+    def getEvents() {
          def events=Event.list()
         return events
 
     }
-    def index() { }
+
+    def makeEvent() {
+        def who = params.who
+        def where = params.where
+        def what = params.what
+        def when = params.when
+
+        render(status: 400, text: "fail!")
+//        render(status: 200, text: "woot!")
+    }
+
 }
