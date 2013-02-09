160acf261fa0d67622a9ceac3efe8ed97ac2ce0d
emmyrlim <emmyrlim@gmail.com>
2013-02-09 02:09:39 -0600

    beta1

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -4,5 +4,6 @@ modules = {
         resource url:'js/moment.js'
         resource url:'js/application.js'
         resource url:'js/jquery-ui-1.10.0.custom.min.js'
+        resource url:'js/kinetic.js'
     }
 }
