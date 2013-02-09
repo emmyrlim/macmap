3721fe887d2aaa426d65a5ffa4e659d2249f00af
JacobS <jstein3@macalester.edu>
2013-02-08 20:02:55 -0800

    EventController

--- grails-app/controllers/org/macmap/EventController.groovy
+++ grails-app/controllers/org/macmap/EventController.groovy
@@ -1,9 +1,18 @@
 package org.macmap
+import grails.converters.JSON
+
 
 class EventController {
     def getEvents(){
          def events=Event.list()
-        return events
+        ArrayList<String> eventList=new ArrayList<String>()
+        for(Event e: events){
+            String s=e.getEventName()+" "+e.getStart()+ " "+e.getEnd()+" "+ e.getPlace()
+            for(Person p: e.getPeople()){
+                s+=" "+p.name
+            }
+        }
+        return eventList as JSON
 
     }
     def index() { }
