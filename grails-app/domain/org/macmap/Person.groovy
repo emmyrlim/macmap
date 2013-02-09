fbd0f202c75f81f00e3dfbf2ca1b36494b34df31
JacobS <jstein3@macalester.edu>
2013-02-08 17:38:59 -0800

    Domain classes

--- grails-app/domain/org/macmap/Person.groovy
+++ grails-app/domain/org/macmap/Person.groovy
@@ -3,5 +3,6 @@ package org.macmap
 class Person {
      String name
     static constraints = {
+        name nullable: false
     }
 }
--- grails-app/domain/org/macmap/Place.groovy
+++ grails-app/domain/org/macmap/Place.groovy
@@ -3,5 +3,6 @@ package org.macmap
 class Place {
     String name
     static constraints = {
+        name nullable: false
     }
 }
