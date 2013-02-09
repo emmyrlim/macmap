5581f617981a1586a8bca33e23fecf641948a389
JacobS <jstein3@macalester.edu>
2013-02-08 17:31:22 -0800

    Domain classes

--- grails-app/domain/org/macmap/Event.groovy
+++ grails-app/domain/org/macmap/Event.groovy
@@ -1,8 +1,15 @@
 package org.macmap
 
 class Event {
-
+    String eventName
+    Date start
+    Date end
+    Place place
     static constraints = {
+        people minSize: 1
+        start nullable: false
+        end nullable: false
+        place nullable: false
     }
     static hasMany=[people: Person]
 }
